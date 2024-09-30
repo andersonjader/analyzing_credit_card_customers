89fd70d1d438a6b60a5093e749c282fad4808224
andersonjader <ajaderantunes@gmail.com>
2024-09-30 17:15:11 -0300

    add calculo de medias

--- src/Notebooks/utils/general.py
+++ src/Notebooks/utils/general.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from pandas import *
+import pandas as pd
 
 
 def catchPath(arq):
@@ -17,3 +18,18 @@ def SaveDataFrame(df, name):
     n = 'data/' + name + '.xlsx'
     p = catchPath(n)
     df.to_excel(p, index = False)
+
+def make_dataframe(serie, nome, nome_novo):
+    """
+    função que cria um dataframe a partir de uma serie que foi gerada de um slice de uma lina
+    recebe como parametro: a serie, o nome da coluna atual e depois o nome novo nome
+    """
+
+    d = pd.DataFrame(serie)
+    d = d.rename(columns={nome:nome_novo})
+    d = d.transpose()
+    return d
+
+def concat_dataframes(frames):
+    d = pd.concat(frames)
+    return d
--- src/Notebooks/utils/norm_test.py
+++ src/Notebooks/utils/norm_test.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from scipy import stats
+from scipy.stats import anderson
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -13,6 +14,15 @@ def QQplot(data):
 def calcShapiro(data):
     r = stats.shapiro(data)
     return r[1]
+
+def cal_Anderson_Darling(data,a):
+    p = anderson(data).statistic
+    if p < a:
+        r = 'rejects the null hypothesis'
+        return r
+    else:
+        r = 'fails to reject the null hypothesis'
+        return r
 
 def distribuitionGaus(data, name):
     #densidade, método kdeplot para densidade
